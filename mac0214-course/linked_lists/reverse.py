85179d0d58ff353c6cdcee18bbf2e47baf23f34a
mateuslatrova <mateuslatrova@usp.br>
2023-09-03 12:14:36 -0300

    add solution for "reverse linked list" problem

---
+++ mac0214-course/linked_lists/reverse.py
@@ -0,0 +1,23 @@
+# Problem: https://leetcode.com/problems/valid-palindrome/
+# Status   / Language / Runtime / Memory  /  Time
+# Accepted / Python   /  46ms   / 17.79MB /  a few seconds ago
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        curr_node = head
+        prev_node = None
+        while curr_node != None:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+
+        return prev_node
